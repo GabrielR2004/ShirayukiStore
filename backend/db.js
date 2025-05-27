445c4afaa31d67a6b72ebbcb3594519a69c13690
GabrielR2004 <gabrielrodrigo252004@gmail.com>
2025-05-27 18:26:48 -0300

    Subindo projeto

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_RU7Yk8CtVsKn@ep-curly-lake-ac5t0zsu-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
-});
-
-module.exports = pool;
